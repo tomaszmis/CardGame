b1cea09ffe09c6429d827e6ea35e246a6deec579
tomaszmis <49452844+tomaszmis@users.noreply.github.com>
2019-05-04 13:44:30 +0200

    Add all coments

--- src/Eights.java
+++ src/Eights.java
@@ -4,15 +4,29 @@ import java.util.Scanner;
  * 
  * @author Tomasz Miœ
  * @version 1.0
- *
+ * Main class which provides essential methods to play a Eights.
  */
 public class Eights {
+	/**
+	 * Variable which store each player in game.
+	 */
 	private Players players;
+	/**
+	 * Store pile of cards from which players get card in case that don't have proper card in his pile.
+	 */
 	private Hand drawPile;
+	/**
+	 * Store pile of cards which was throw by players, last card on pile determine next move.
+	 */
 	private Hand discardPile;
+	/**
+	 * Thanks <code>in</code> program read number of players and in simulation read next move (Enter).
+	 */
 	private Scanner in;
 	
-	
+	/**
+	 * Constructor <code>Eights</code> create deck for each player and draw pile and discard pile.
+	 */
 	public Eights() {
 		Deck deck = new Deck("Talia");
 		deck.shuffle();
@@ -34,7 +48,7 @@ public class Eights {
 		in = new Scanner(System.in);
 	}
 	/*
-	 * Get from user count of players, if number will be less then two, return 2, else return count;
+	 * Get from user count of players, if number from user will be less then two, return 2, else return count;
 	 */
 	public int setNumberOfPlayers() {
 		in = new Scanner(System.in);
@@ -42,7 +56,9 @@ public class Eights {
 		int Number = in.nextInt();
 		return (Number < 2) ? 2 : Number;
 	}
-	
+	/**
+	 * Decide if the game was ended.
+	 */
 	public boolean isDone() {
 		for(int i = 0; i < players.size(); ++i) {
 			if(players.getPlayer(i).getHand().empty()) {
@@ -51,14 +67,19 @@ public class Eights {
 		}
 		return false;
 	}
-	
+	/**
+	 * If draw pile is empty, cards from discard pile will be moved to draw pile.
+	 */
 	public void reshuffle() {
 		Card prev = discardPile.popCard();
 		discardPile.dealAll(drawPile);
 		discardPile.addCard(prev);
 		drawPile.shuffle();
 	}
-	
+	/**
+	 * Remove card draw pile.
+	 * @return Card from draw pile.
+	 */
 	public Card draw() {
 		if(drawPile.empty()) {
 			reshuffle();
@@ -67,7 +88,11 @@ public class Eights {
 	}
 	
 	
-	
+	/**
+	 * Switch player in game.
+	 * @param current - player who last push card on discard pile.
+	 * @return - next player
+	 */
 	public Player nextPlayer(Player current) {
 		
 		int currentIndex = players.getIndex(current);
@@ -77,7 +102,9 @@ public class Eights {
 			return players.getPlayer(currentIndex + 1);
 		}
 	}
-	
+	/**
+	 * Display state each player in console.
+	 */
 	public void displayState() {
 		for(int i = 0; i < players.size(); ++i) {
 			players.getPlayer(i).display();
@@ -88,11 +115,16 @@ public class Eights {
 		System.out.println("Stos ci¹gniêcia:");
 		System.out.println(drawPile.size() + " kart");
 	}
-	
+	/**
+	 * Wait for <code>Enter</code> in console.
+	 */
 	public void waitForUser() {
 		in.nextLine();
 	}
-	
+	/**
+	 * Turn current player, bring {@link #players.play}
+	 * @param player = current player
+	 */
 	public void takeTurn(Player player) {
 		Card prev = discardPile.last();
 		Card next = player.play(this, prev);
@@ -101,7 +133,9 @@ public class Eights {
 		System.out.println(player.getName() + " gra " + next);
 		System.out.println();
 	}
-	
+	/**
+	 * Main method in which play a Game.
+	 */
 	public void playGame() {
 		Player player = players.getPlayer(0);
 		
--- src/Player.java
+++ src/Player.java
@@ -1,13 +1,32 @@
-
+/**
+ * 
+ * @author Tomasz Miœ
+ * @version 1.0
+ * Represent one player and provide methods to play in Eights.
+ */
 public class Player {
+	/**
+	 * Name of player
+	 */
 	private String name;
+	/**
+	 * Store player's card
+	 */
 	protected Hand hand;
-	
+	/**
+	 * Create new player
+	 * @param name - name of player
+	 */
 	public Player(String name) {
 		this.name = name;
 		this.hand = new Hand(name);
 	}
-	
+	/**
+	 * Method which finally return card to discard pile.
+	 * @param eights - current game
+	 * @param prev - last card on discard pile
+	 * @return card from hand deck to discard pile
+	 */
 	public Card play(Eights eights, Card prev) {
 		
 		Card card = searchForEight();
@@ -19,7 +38,10 @@ public class Player {
 		}
 		return card;
 	}
-	
+	/**
+	 * Looking for card which rank is 8, if is return it else return null.
+	 * @return
+	 */
 	public Card searchForEight() {
 		for(int i = 0; i < hand.size(); ++i) {
 			Card card = hand.getCard(i);
@@ -29,7 +51,11 @@ public class Player {
 		}
 		return null;
 	}
-	
+	/**
+	 * Looking for card which match to last card in discard pile id est this same rank or suit.
+	 * @param prev
+	 * @return
+	 */
 	public Card searchForMatch(Card prev) {
 		for(int i = 0; i < hand.size(); ++i) {
 			Card card = hand.getCard(i);
@@ -39,7 +65,12 @@ public class Player {
 		}
 		return null;
 	}
-	
+	/**
+	 * If player don't have match card in hand he must draw from draw pile while he don't get match card, each drawed card was add to hand cards.
+	 * @param eights - current game
+	 * @param prev - last card on discard pile
+	 * @return - matches card
+	 */
 	public Card drawForMatch(Eights eights, Card prev) {
 		while(true) {
 			Card card = eights.draw();
@@ -50,7 +81,12 @@ public class Player {
 			hand.addCard(card);
 		}
 	}
-	
+	/**
+	 * Decided if card matches to last card in discard pile id est have this same rank or suit or in hand is card which rank is 8.
+	 * @param card1
+	 * @param card2
+	 * @return
+	 */
 	public static boolean cardMatches(Card card1, Card card2) {
 		if(card1.getSuit() == card2.getSuit()) {
 			return true;
@@ -63,7 +99,10 @@ public class Player {
 		}
 		return false;
 	}
-	
+	/**
+	 * Calculate point each player. 
+	 * @return sum of points
+	 */
 	public int score() {
 		int sum = 0;
 		for(int i = 0; i < hand.size(); ++i) {
@@ -79,19 +118,29 @@ public class Player {
 		}
 		return sum;
 	}
-	
+	/**
+	 * Write in console score.
+	 */
 	public void displayScore() {
 		System.out.println("Wynik: " + name + " to " + score());
 	}
-	
+	/**
+	 * Display all information about player name, count of cards in hand and score.
+	 */
 	public void display() {
 		System.out.println("Gracz " + name + " Liczba kart w rêce: " + hand.size() + " Wynik to " + score());
 	}
-	
+	/**
+	 * Getter return name of player.
+	 * @return - name of player.
+	 */
 	public String getName() {
 		return name;
 	}
-	
+	/**
+	 * Getter return players cards.
+	 * @return - players cards.
+	 */
 	public Hand getHand() {
 		return hand;
 	}
